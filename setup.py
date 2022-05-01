86c5b00a92aac35ac3b09cf13f79323e9d34bfae
mapl <placenti.marco@gmail.com>
2022-05-01 09:55:02 +0200

    changed release version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='DeePipe',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.0.1',
     description='DL Pipeline',
     author='MOPC',
     license='MIT',
