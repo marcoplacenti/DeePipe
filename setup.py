eb1087bf22443c09dc7897b5b792e400122422aa
mapl <placenti.marco@gmail.com>
2022-05-01 09:40:40 +0200

    fixed logic error in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='deepipe',
+    name='DeePipe',
     packages=find_packages(),
     version='0.1.0',
     description='DL Pipeline',
     author='MOPC',
     license='MIT',
-    project_urls='https://github.com/marcoplacenti/MLPipe'
+    project_urls={
+        "Source Code": 'https://github.com/marcoplacenti/DeePipe'
+    }
 )
